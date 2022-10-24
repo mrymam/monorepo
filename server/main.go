7759462d562d292c8ee15dbcd62b2b577824a907
onyanko-pon <engineer.tsukasa@gmail.com>
2022-10-25 06:15:21 +0900

    init: echo

--- server/main.go
+++ server/main.go
@@ -1,6 +1,26 @@
 package main
-import "fmt"
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-	fmt.Println("hello")
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
