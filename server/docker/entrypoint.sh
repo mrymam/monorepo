ef0a265901a3c36a345e87d5cb29becbfef817e9
onyanko-pon <engineer.tsukasa@gmail.com>
2022-10-26 11:00:11 +0900

    add: migrate to entrypoint

--- server/docker/entrypoint.sh
+++ server/docker/entrypoint.sh
@@ -6,4 +6,6 @@ DATABASE_FILE_BACKET_FULLPATH="gcs://bucket_name/db/prod.sqlite3"
 EXEC_COMMAND="/go/bin/server"
 
 litestream restore -v -if-replica-exists -o "$DATABASE_FILE_FULLPATH" "$DATABASE_FILE_BACKET_FULLPATH"
+
+cat /sqlite/migrations/*.sql | sqlite3 $DATABASE_FILE_FULLPATH
 exec litestream replicate -exec "$EXEC_COMMAND" -config /etc/litestream.yml
