6a853413f4673a39082db604ef93a22ea27ec509
onyanko-pon <engineer.tsukasa@gmail.com>
2022-10-27 12:06:02 +0900

    fix: Dockerfile

--- server/docker/entrypoint.sh
+++ server/docker/entrypoint.sh
@@ -1,11 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash
+set -e
 
-DATABASE_FILE_FULLPATH="/server/sqlite/prod.sqlite3"
-DATABASE_FILE_BACKET_FULLPATH="gcs://bucket_name/db/prod.sqlite3"
+SQLITE_FILE_BACKET_FULLPATH="gcs://prod-monorepo-sqlite/db/prod.sqlite3"
 
 EXEC_COMMAND="/go/bin/server"
 
-litestream restore -v -if-replica-exists -o "$DATABASE_FILE_FULLPATH" "$DATABASE_FILE_BACKET_FULLPATH"
+litestream restore -v -if-replica-exists -o "${SQLITE_FILEPATH}" "${SQLITE_FILE_BACKET_FULLPATH}"
 
-cat /sqlite/migrations/*.sql | sqlite3 $DATABASE_FILE_FULLPATH
-exec litestream replicate -exec "$EXEC_COMMAND" -config /etc/litestream.yml
+cat /sqlite/migrations/*.sql | sqlite3 ${SQLITE_FILEPATH}
+exec litestream replicate -exec "/go/bin/server" -config /usr/bin/litestream.yml
