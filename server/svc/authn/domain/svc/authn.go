64d42022edc2f644a024519dc94b87e5bdfe7cb8
onyanko-pon <engineer.tsukasa@gmail.com>
2022-10-28 14:08:56 +0900

    add: userid value obj

--- server/svc/authn/domain/svc/authn.go
+++ server/svc/authn/domain/svc/authn.go
@@ -1,9 +1,13 @@
 package svc
 
+import (
+	"github.com/onyanko-pon/monorepo/server/svc/authn/domain/model"
+)
+
 type VerifyToken interface {
 	Do(tkn string) (Payload, bool, error)
 }
 
 type Payload struct {
-	UserID string
+	UserID model.UserID
 }
